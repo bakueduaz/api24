532cba277e3efa15c14875cf3ddb257b1db6817b
Nihad Nahmatli <bakueduaz@gmail.com>
2024-10-07 05:12:03 +0000

    changed

--- api/models.py
+++ api/models.py
@@ -6,19 +6,19 @@ class News:
         self.des = des
         self.img = img
 
-news1 = (
+news1 = News(
     "Ray-Ban Meta",
     "You can now use the glasses to remind you to make a phone call or even where you parked your car.",
     "https://picsum.photos/400?random=1"
 )
 
-news2 = (
+news2 = News(
     "Amazon’s new Fire tablet",
     "The company’s refreshed Fire HD 8 will get some new AI tools, but they’re rolling out to other Amazon tablets, too.",
     "https://picsum.photos/400?random=2"
 )
 
-news3 = (
+news3 = News(
     "Meta unveils AI video tool: Movie Gen",
     "Movie Gen can create new clips from text prompts or edit your face into existing footage — complete with sound.",
     "https://picsum.photos/400?random=3"
